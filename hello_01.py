20652c8b85c99d0571fa038aa09053ac11a24a2b
ProgrammersGit <Krishankant94185@gmail.com>
2023-04-14 12:43:19 +0530

    Initial commit

---
+++ hello_01.py
@@ -0,0 +1,10 @@
+# Author : ProgrammersGit # Single line comment ...
+'''
+Licenced to : ABC Industries
+by ProgrammersGit 
+''' 
+# Multiline comment ...
+
+print( " Hello Python ") #This will print hello python ....
+
+# Thanks for Visiting ProgrammersGit :)
